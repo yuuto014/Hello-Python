7fb934abfe20ed535aa5e89b8e0fce9a95171a3b
W4Sticks-Yuuto <larrota.juancamilo@gmail.com>
2022-09-10 16:07:38 -0500

    Clase 1 (dias 1 y 2) del 30-days-of-python

---
+++ 00_hello.py
@@ -0,0 +1,14 @@
+#Hola mundo 
+print("Hola Python")
+print('Hola Python')
+"""
+Comentario de varias 
+lineas
+"""
+
+#Consulrar del tipo de dato
+print(type("Esta es una cadena de texto")) #Tipo 'str'
+print(type(4)) #Tipo 'int'
+print(type(1.5)) #Tipo 'float'
+print(type(3 + 4j)) #Tipo 'complex'
+print(type(True)) #Tipo 'bool'
--- 01_variables.py
+++ 01_variables.py
@@ -1,46 +1,44 @@
-# Clase 1 (03/08/22) en directo desde Twitch: https://www.twitch.tv/videos/1551265068
-
-### Variables ###
-
-my_string_variable = "My String variable"
+#Variables
+my_string_variable = "My String variable" 
 print(my_string_variable)
 
 my_int_variable = 5
 print(my_int_variable)
 
+my_bool_variable = False
+print(my_bool_variable)
+
+#Variables en una sola linea
+name, surname, alias, age = "Juan", "Larrota", "Yuuto", 26
+print("mi nombres es:",name,surname,". Mi edad es: ",age,"y mi alias es:",alias)
+
+#Cambiar el tipo de variable
 my_int_to_str_variable = str(my_int_variable)
 print(my_int_to_str_variable)
 print(type(my_int_to_str_variable))
 
-my_bool_variable = False
-print(my_bool_variable)
+#Concatenacion de variables en un print
+print(my_string_variable, my_int_variable, my_bool_variable)
+print("Este es el valor de: ",my_bool_variable)
 
-# Concatenación de variables en un print
-print(my_string_variable, my_int_to_str_variable, my_bool_variable)
-print("Este es el valor de:", my_bool_variable)
+#Algunas funciones del sistema
+#len() cuenta la cantidad de caracteres 
+print(len(my_string_variable)) #18 
 
-# Algunas funciones del sistema
-print(len(my_string_variable))
+#Ingreso por pantalla
 
-# Variables en una sola línea. ¡Cuidado con abusar de esta sintaxis!
-name, surname, alias, age = "Brais", "Moure", 'MoureDev', 35
-print("Me llamo:", name, surname, ". Mi edad es:", age, ". Y mi alias es:", alias)
-
-# Inputs
-name = input('¿Cuál es tu nombre? ')
-age = input('¿Cuántos años tienes? ')
+name = input("What is your name?")
+age = input("How old are you?")
 print(name)
 print(age)
 
-# Cambiamos su tipo
-name = 35
-age = "Brais"
+#Cambiamos tipo de datos
+name = 26
+age = "juan"
 print(name)
 print(age)
 
-# ¿Forzamos el tipo?
-address: str = "Mi dirección"
-address = True
-address = 5
-address = 1.2
-print(type(address))
+#Forzamos el tipo
+adress : str = "Esta es mi direccion"
+adress = 32
+print(type(adress))
